c1aff078eaa544b398a6305d2593539ba20e7785
Richard Franks <git@rf152.co.uk>
2024-11-26 10:30:57 +0000

    More documentation

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,20 @@
 #include <SPI.h>  // Library for using SPI Communication
 #include <mcp2515.h>  // Library for using CAN Communication
 
+// Chip select pin used for MCP2515
 #define CS_PIN 10
-#define CAN_BAUDRATE 500000
+// Baud rate of the CANBUS
+#define CAN_BAUDRATE 500e3
 
-// This should be calibrated using a reliable voltage source
+// Millivolts per step in the ADC.
+// This value should be calibrated on a per-board basis, as it
+// will depend on the resistors
 #define MILLIVOLTSPERSTEP 29.3
 
+// Status LED output
 #define STATUS_LED 4
+
+// 24v input sense pin
 #define SENSE_24V A0
 
 // A single CAN frame, re-used to send can messages
