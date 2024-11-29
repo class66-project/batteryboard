a91697b87f3854a83e188e4a18fa3208e7118efd
Richard Franks <git@rf152.co.uk>
2024-11-29 17:11:35 +0000

    Refactored JKBMS into class

--- src/jkbms.cpp
+++ src/jkbms.cpp
@@ -13,80 +13,155 @@
 #include "definitions.h"
 #include "statusled.h"
 #include "can.h"
+#include "can-protocol/sensors.h"
 
 SoftwareSerial mySerial =  SoftwareSerial(5, 6);
 
-void runBmsSensors() {
-  mySerial.begin(9600);
-  uint8_t data[308];
-  flashStatusLED();
-  if (_getBmsResponse(data) != 308) return;
+JKBMS bms;
 
-  // Check the header
-  if (
-    data[0] != 0x55 ||
-    data[1] != 0xAA ||
-    data[2] != 0xEB ||
-    data[3] != 0x90 ||
-    data[4] != 0x02
-  ) {
-    return;
-  }
+void runBmsSensors() {
+  bms.run();
+}
+
+JKBMS::JKBMS() {
+  this->setSerial(&mySerial);
+}
+
+void JKBMS::setSerial(SoftwareSerial *serial) {
+  this->_serial = serial;
+}
+
+void JKBMS::run() {
+  this->_serial->begin(9600);
+  flashStatusLED();
+  if (this->_getBmsResponse() != 308) return;
+
+  if (!this->_checkHeader()) return;
 
   // Get the count of cells
-  uint8_t cellCount = _getCellCount(data);
-  uint16_t avgVoltage = _getCellAverage(data);
-  uint16_t deviation = _getCellDeviation(data);
+  uint8_t cellCount = this->_getCellCount();
+  uint16_t avgVoltage = this->_getCellAverage();
+  uint16_t deviation = this->_getCellDeviation();
 
   uint8_t messageData[8];
-  messageData[0] = 0xC0;
+  messageData[0] = CELL_COUNT;
   messageData[1] = cellCount;
-  messageData[2] = 0x02;
+  messageData[2] = VOLTAGE_CELL_AVERAGE;
   messageData[3] = avgVoltage >> 8;
   messageData[4] = (uint8_t) avgVoltage;
-  messageData[5] = 0x03;
+  messageData[5] = VOLTAGE_CELL_DEVIATION;
   messageData[6] = deviation >> 8;
   messageData[7] = (uint8_t) deviation;
 
   sendSensors(messageData, 8);
 
-  messageData[0] = 0xC7;
-  messageData[1] = _getHighestCell(data) + 1;
-  messageData[2] = 0xC8;
-  messageData[3] = _getLowestCell(data) + 1;
+  messageData[0] = HIGHEST_CELL;
+  messageData[1] = this->_getHighestCell() + 1;
+  messageData[2] = LOWEST_CELL;
+  messageData[3] = this->_getLowestCell() + 1;
 
   sendSensors(messageData, 4);
 
   // Send off the cell voltages over CAN
-  _sendCellVoltages(data, cellCount);
-  _sendCellWireResitance(data, cellCount);
+  this->_sendCellVoltages(cellCount);
+  this->_sendCellWireResitance(cellCount);
 
-  uint32_t overallVoltage = _getPackVoltage(data);
-  messageData[0] = 0x01;
+  uint32_t overallVoltage = this->_getPackVoltage();
+  messageData[0] = VOLTAGE_MAIN;
   messageData[1] = overallVoltage >> 24;
   messageData[2] = overallVoltage >> 16;
   messageData[3] = overallVoltage >> 8;
   messageData[4] = (uint8_t)overallVoltage;
   sendSensors(messageData, 5);
 
-  int32_t overallPower = _getPower(data);
-  messageData[0] = 0xC1;
+  int32_t overallPower = this->_getPower();
+  messageData[0] = POWER_MAIN;
   messageData[1] = overallPower >> 24;
   messageData[2] = overallPower >> 16;
   messageData[3] = overallPower >> 8;
   messageData[4] = (uint8_t)overallPower;
   sendSensors(messageData, 5);
 
-  int32_t overallCurrent = _getCurrent(data);
-  messageData[0] = 0x20;
+  int32_t overallCurrent = this->_getCurrent();
+  messageData[0] = CURRENT_MAIN;
   messageData[1] = overallCurrent >> 24;
   messageData[2] = overallCurrent >> 16;
   messageData[3] = overallCurrent >> 8;
   messageData[4] = (uint8_t)overallCurrent;
   sendSensors(messageData, 5);
+
+  uint8_t remainingCapacityPercent = this->_getRemainingCapacityPercent();
+  uint16_t remainingCapacityAmpHours = this->_getRemainingCapacityAmpHours();
+
+  messageData[0] = CAPACITY_REMAINING_PERCENT;
+  messageData[1] = remainingCapacityPercent;
+  messageData[2] = CAPACITY_REMAINING_AMP_HOURS;
+  messageData[3] = remainingCapacityAmpHours >> 8;
+  messageData[4] = (uint8_t) remainingCapacityAmpHours;
+  sendSensors(messageData, 5);
+
+  uint32_t nominalCapacity = this->_getNominalCapacity();
+  messageData[0] = CAPACITY_NOMINAL;
+  messageData[1] = nominalCapacity >> 24;
+  messageData[2] = nominalCapacity >> 16;
+  messageData[3] = nominalCapacity >> 8;
+  messageData[4] = (uint8_t)nominalCapacity;
+  messageData[5] = CHARGE_ENABLED;
+  messageData[6] = this->_getChargeEnabled();
+  sendSensors(messageData, 7);
+
+  uint32_t cycleCount = this->_getCycleCount();
+  messageData[0] = CYCLE_COUNT;
+  messageData[1] = cycleCount >> 24;
+  messageData[2] = cycleCount >> 16;
+  messageData[3] = cycleCount >> 8;
+  messageData[4] = (uint8_t)cycleCount;
+  messageData[5] = DISCHARGE_ENABLED;
+  messageData[6] = this->_getDischargeEnabled();
+  sendSensors(messageData, 7);
+
+  uint16_t temperatures[5];
+  temperatures[0] = this->_get16BitValue(temperature1);
+  temperatures[1] = this->_get16BitValue(temperature2);
+  temperatures[2] = this->_get16BitValue(temperature3);
+  temperatures[3] = this->_get16BitValue(tempearture4);
+  temperatures[4] = this->_get16BitValue(temperature5);
+
+  messageData[0] = TEMPERATURE_SENSOR_1;
+  messageData[1] = temperatures[0] >> 8;
+  messageData[2] = (uint8_t)temperatures[0];
+  messageData[3] = TEMPERATURE_SENSOR_2;
+  messageData[4] = temperatures[1] >> 8;
+  messageData[5] = (uint8_t)temperatures[1];
+  sendSensors(messageData, 6);
+  messageData[0] = TEMPERATURE_SENSOR_3;
+  messageData[1] = temperatures[2] >> 8;
+  messageData[2] = (uint8_t)temperatures[2];
+  messageData[3] = TEMPERATURE_SENSOR_4;
+  messageData[4] = temperatures[3] >> 8;
+  messageData[5] = (uint8_t)temperatures[3];
+  sendSensors(messageData, 6);
+  messageData[0] = TEMPERATURE_SENSOR_5;
+  messageData[1] = temperatures[2] >> 8;
+  messageData[2] = (uint8_t)temperatures[4];
+  sendSensors(messageData, 3);
 }
 
-uint32_t _getBmsResponse(uint8_t* data) {
+bool JKBMS::_checkHeader() {
+  // Check the header
+  if (
+    this->_data[0] != 0x55 ||
+    this->_data[1] != 0xAA ||
+    this->_data[2] != 0xEB ||
+    this->_data[3] != 0x90 ||
+    this->_data[4] != 0x02
+  ) {
+    return false;
+  }
+  return true;
+}
+
+uint32_t JKBMS::_getBmsResponse() {
   // The trigger data to send to the BMS in order for it to send the data back
   uint8_t _jktrigger[11] =
         {0x01, 0x10, 0x16, 0x20, 0x00, 0x01, 0x02, 0x00, 0x00, 0xD6, 0xF1};
@@ -96,20 +171,20 @@ uint32_t _getBmsResponse(uint8_t* data) {
   delay(10);
   // Send the 11 bytes of data
   for (int i = 0; i < 11; i++) {
-    mySerial.write(_jktrigger[i]);
-    mySerial.flush();
+    this->_serial->write(_jktrigger[i]);
+    this->_serial->flush();
   }
   delay(10);
   // Put the MAX485 back in RE mode
   digitalWrite(DE, 0);
 
   // Return the number of bytes read back
-  return mySerial.readBytes(data, 308);
+  return this->_serial->readBytes(this->_data, 308);
 }
 
-uint8_t _getCellCount(uint8_t *data) {
+uint8_t JKBMS::_getCellCount() {
   // Combine the four bytes into a single uint32_t
-  uint32_t bitmask = _get32BitValue(data, 70);
+  uint32_t bitmask = _get32BitValue(enabledCells);
 
   // Compare against the known values
   switch (bitmask) {
@@ -145,7 +220,7 @@ uint8_t _getCellCount(uint8_t *data) {
   }
 }
 
-void _sendCellVoltages(uint8_t* data, uint8_t cellCount) {
+void JKBMS::_sendCellVoltages(uint8_t cellCount) {
   // Work out how many messages to build
   uint8_t runCount = floor(cellCount / 2);
 
@@ -157,8 +232,8 @@ void _sendCellVoltages(uint8_t* data, uint8_t cellCount) {
       // Sensor address
       voltages[(j * 3)] = 0x10 + (i * 2) + j;
       // The bytes of the value
-      voltages[(j * 3) + 1] = data[6 + (i * 4) + (j * 2)];
-      voltages[(j * 3) + 2] = data[7 + (i * 4) + (j * 2)];
+      voltages[(j * 3) + 1] = this->_data[cellVoltages + (i * 4) + (j * 2)];
+      voltages[(j * 3) + 2] = this->_data[cellVoltages + 1 + (i * 4) + (j * 2)];
     }
 
     sendSensors(voltages, 6);
@@ -167,14 +242,14 @@ void _sendCellVoltages(uint8_t* data, uint8_t cellCount) {
   // Catch the instance where there is an odd number of cells
   if (cellCount % 2 > 0) {
     voltages[0] = 0x10 + runCount * 2;
-    voltages[1] = data[6 + runCount * 4];
-    voltages[2] = data[7 + runCount * 4];
+    voltages[1] = this->_data[cellVoltages + runCount * 4];
+    voltages[2] = this->_data[cellVoltages + 1 + runCount * 4];
 
     sendSensors(voltages, 3);
   }
 }
 
-void _sendCellWireResitance(uint8_t* data, uint8_t cellCount) {
+void JKBMS::_sendCellWireResitance(uint8_t cellCount) {
   // Work out how many messages to build
   uint8_t runCount = floor(cellCount / 2);
 
@@ -186,8 +261,8 @@ void _sendCellWireResitance(uint8_t* data, uint8_t cellCount) {
       // Sensor address
       resistances[(j * 3)] = 0xD0 + (i * 2) + j;
       // The bytes of the value
-      resistances[(j * 3) + 1] = data[80 + (i * 4) + (j * 2)];
-      resistances[(j * 3) + 2] = data[81 + (i * 4) + (j * 2)];
+      resistances[(j * 3) + 1] = this->_data[cellWireResistances + (i * 4) + (j * 2)];
+      resistances[(j * 3) + 2] = this->_data[cellWireResistances + 1 + (i * 4) + (j * 2)];
     }
     // Send it off
     sendSensors(resistances, 6);
@@ -196,65 +271,83 @@ void _sendCellWireResitance(uint8_t* data, uint8_t cellCount) {
   // Catch the instance where there is an odd number of cells
   if (cellCount % 2 > 0) {
     resistances[0] = 0xD0 + runCount * 2;
-    resistances[1] = data[80 + runCount * 4];
-    resistances[2] = data[81 + runCount * 4];
+    resistances[1] = this->_data[cellWireResistances + runCount * 4];
+    resistances[2] = this->_data[cellWireResistances + 1 + runCount * 4];
     sendSensors(resistances, 3);
   }
 }
 
-uint8_t _getHighestCell(uint8_t *data) {
-  return _get8BitValue(data, 78);
+uint8_t JKBMS::_getHighestCell() {
+  return this->_get8BitValue(cellHighest);
 }
 
-uint8_t _getLowestCell(uint8_t *data) {
-  return _get8BitValue(data, 79);
+uint8_t JKBMS::_getLowestCell() {
+  return this->_get8BitValue(cellLowest);
 }
 
-uint16_t _getCellDeviation(uint8_t *data) {
-  return _get16BitValue(data, 74);
+uint16_t JKBMS::_getCellDeviation() {
+  return this->_get16BitValue(cellDeviation);
 }
 
-uint16_t _getCellAverage(uint8_t *data) {
-  return _get16BitValue(data, 74);
+uint16_t JKBMS::_getCellAverage() {
+  return this->_get16BitValue(cellAverageVoltage);
 }
 
-uint32_t _getPackVoltage(uint8_t *data) {
-  return _get32BitValue(data, 150);
+uint32_t JKBMS::_getPackVoltage() {
+  return this->_get32BitValue(packVoltage);
 }
 
-int32_t _getPower(uint8_t *data) {
-  return _get32BitValue(data, 154);
+int32_t JKBMS::_getPower() {
+  return _get32BitValue(packPower);
 }
 
-int32_t _getCurrent(uint8_t *data) {
-  return _get32BitValue(data, 158);
+int32_t JKBMS::_getCurrent() {
+  return this->_get32BitValue(packCurrent);
 }
 
-uint16_t _getRemainingCapacity(uint8_t *data);
-uint32_t _getNominalCapacity(uint8_t *data);
-uint32_t _getCycleCount(uint8_t *data);
-bool _getChargeEnabled(uint8_t *data);
-bool _getDischargeEnabled(uint8_t *data);
-
-uint16_t* _getTemperatures(uint8_t *data);
-
-int8_t _get8BitValue(uint8_t *data, uint16_t offset) {
-  return data[offset];
+uint8_t JKBMS::_getRemainingCapacityPercent() {
+  return this->_get8BitValue(packCapacityRemainingPercentage);
 }
 
-int16_t _get16BitValue(uint8_t *data, uint16_t offset) {
+uint16_t JKBMS::_getRemainingCapacityAmpHours() {
+  return this->_get16BitValue(packCapacityRemainingAmpHours);
+}
+
+uint32_t JKBMS::_getNominalCapacity() {
+  return this->_get32BitValue(nominalCapacity);
+}
+
+uint32_t JKBMS::_getCycleCount() {
+  return this->_get32BitValue(cycleCount);
+}
+
+bool JKBMS::_getChargeEnabled() {
+  uint8_t chargeEnabled = this->_get8BitValue(chargeMosfetEnabled);
+  return chargeEnabled == 1;
+}
+
+bool JKBMS::_getDischargeEnabled() {
+  uint8_t chargeEnabled = this->_get8BitValue(dischargeMosfetEnabled);
+  return chargeEnabled == 1;
+}
+
+int8_t JKBMS::_get8BitValue(uint16_t offset) {
+  return this->_data[offset];
+}
+
+int16_t JKBMS::_get16BitValue(uint16_t offset) {
   int16_t ret = 0;
-  ret |= (int16_t)data[offset] << 8;
-  ret |= (int16_t)data[offset + 1];
+  ret |= (int16_t)this->_data[offset] << 8;
+  ret |= (int16_t)this->_data[offset + 1];
   return ret;
 }
 
-int32_t _get32BitValue(uint8_t *data, uint16_t offset) {
+int32_t JKBMS::_get32BitValue(uint16_t offset) {
   int32_t ret = 0;
 
-  ret |= (int32_t)data[offset] << 24;
-  ret |= (int32_t)data[offset + 1] << 16;
-  ret |= (int32_t)data[offset + 2] << 8;
-  ret |= (int32_t)data[offset + 3];
+  ret |= (int32_t)this->_data[offset] << 24;
+  ret |= (int32_t)this->_data[offset + 1] << 16;
+  ret |= (int32_t)this->_data[offset + 2] << 8;
+  ret |= (int32_t)this->_data[offset + 3];
   return ret;
 }
--- src/jkbms.h
+++ src/jkbms.h
@@ -7,35 +7,88 @@
  */
 
 #pragma once
+#include <SoftwareSerial.h>
 #include "arduino-mcp2515/mcp2515.h"
-
-
 
 void runBmsSensors();
 
-uint32_t _getBmsResponse(uint8_t* data);
+class JKBMS {
+ private:
+  uint8_t _data[308];
+  SoftwareSerial *_serial;
 
-uint8_t _getCellCount(uint8_t *data);
+  enum OFFSETS : uint16_t {
+    cellVoltages = 6,
+    enabledCells = 70,
+    cellAverageVoltage = 74,
+    cellDeviation = 76,
+    cellHighest = 78,
+    cellLowest = 79,
+    cellWireResistances = 80,
+    mosTemp = 144,
+    packVoltage = 150,
+    packPower = 154,
+    packCurrent = 158,
+    temperature1 = 162,
+    temperature2 = 164,
+    alarms1 = 166,
+    alarms2 = 167,
+    alarms3 = 168,
+    balanceCurrent = 170,
+    balancingAction = 172,
+    packCapacityRemainingPercentage = 173,
+    packCapacityRemainingAmpHours = 174,
+    nominalCapacity = 178,
+    cycleCount = 182,
+    SOCSOH = 190,
+    userAlarm = 192,
+    totalRuntime = 194,
+    chargeMosfetEnabled = 198,
+    dischargeMosfetEnabled = 199,
+    timeDcOCPR = 200,
+    timeDcSCPR = 202,
+    timeCOCPR = 204,
+    timeCSCPR = 206,
+    timeUVPR = 208,
+    timeOVPR = 210,
+    temperature3 = 254,
+    tempearture4 = 256,
+    temperature5 = 258,
+  };
 
-void _sendCellVoltages(uint8_t* data, uint8_t cellCount);
-void _sendCellWireResitance(uint8_t* data, uint8_t cellCount);
-uint8_t _getHighestCell(uint8_t *data);
-uint8_t _getLowestCell(uint8_t *data);
-uint16_t _getCellDeviation(uint8_t *data);
-uint16_t _getCellAverage(uint8_t *data);
+ public:
+  JKBMS();
 
-uint32_t _getPackVoltage(uint8_t *data);
-int32_t _getPower(uint8_t *data);
-int32_t _getCurrent(uint8_t *data);
-uint16_t _getRemainingCapacity(uint8_t *data);
-uint32_t _getNominalCapacity(uint8_t *data);
-uint32_t _getCycleCount(uint8_t *data);
-bool _getChargeEnabled(uint8_t *data);
-bool _getDischargeEnabled(uint8_t *data);
+  void setSerial(SoftwareSerial *serial);
 
-uint16_t* _getTemperatures(uint8_t *data);
+  void run();
+
+ private:
+  uint32_t _getBmsResponse();
+  bool _checkHeader();
+
+  uint8_t _getCellCount();
+
+  void _sendCellVoltages(uint8_t cellCount);
+  void _sendCellWireResitance(uint8_t cellCount);
+  uint8_t _getHighestCell();
+  uint8_t _getLowestCell();
+  uint16_t _getCellDeviation();
+  uint16_t _getCellAverage();
+
+  uint32_t _getPackVoltage();
+  int32_t _getPower();
+  int32_t _getCurrent();
+  uint8_t _getRemainingCapacityPercent();
+  uint16_t _getRemainingCapacityAmpHours();
+  uint32_t _getNominalCapacity();
+  uint32_t _getCycleCount();
+  bool _getChargeEnabled();
+  bool _getDischargeEnabled();
+
+  int8_t _get8BitValue(uint16_t offset);
+  int16_t _get16BitValue(uint16_t offset);
+  int32_t _get32BitValue(uint16_t offset);
+};
 
 
-int8_t _get8BitValue(uint8_t *data, uint16_t offset);
-int16_t _get16BitValue(uint8_t *data, uint16_t offset);
-int32_t _get32BitValue(uint8_t *data, uint16_t offset);
